c10988577b818802a49e0c9022b0ff2a2a60cc59
Perrine Martyris <perrinemartyris@gmail.com>
2019-03-27 17:27:26 +0100

    exo state

--- src/App.js
+++ src/App.js
@@ -1,62 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo3.png';
-import './App.css';
-import NewComposant from './NewCompo';
-import Avatar from './Avatar';
-
+import React, { Component } from "react";
+import logo from "./logo3.png";
+import "./App.css";
+import NewComposant from "./NewCompo";
+import Avatar from "./Avatar";
+import Count from "./Count";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <NewComposant />
-          <Avatar 
-          image = 'https://www.supersoluce.com/sites/default/files/styles/exact_size/public/node/127828/kannahommeportrait.png?itok=xraZxxLv'
-          firstName = "Perrine"
-          lastName = "Martyris"
-          />
-          <Avatar 
-          image = 'https://i.pinimg.com/originals/33/c5/2c/33c52c6a2a243bc785df4d2d6efbd9b3.png'
-          firstName = "Toto"
-          lastName = "Gogo"
-          />
-          <Avatar 
-          image = 'https://vignette2.wikia.nocookie.net/dgraymansp/images/6/6f/Allen_Cuerpo_Casi_Completo_(FanArt).png/revision/latest?cb=20130507224558&path-prefix=es'
-          firstName = "Super"
-          lastName = "Man"
-          />
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<NewComposant />
+					<Avatar
+						image="https://www.supersoluce.com/sites/default/files/styles/exact_size/public/node/127828/kannahommeportrait.png?itok=xraZxxLv"
+						firstName="Perrine"
+						lastName="Martyris"
+						star
+					/>
+					<Count />
+					<Avatar
+						image="https://i.pinimg.com/originals/33/c5/2c/33c52c6a2a243bc785df4d2d6efbd9b3.png"
+						firstName="Toto"
+						lastName="Gogo"
+					/>
+					<Avatar
+						image="https://vignette2.wikia.nocookie.net/dgraymansp/images/6/6f/Allen_Cuerpo_Casi_Completo_(FanArt).png/revision/latest?cb=20130507224558&path-prefix=es"
+						firstName="Super"
+						lastName="Man"
+					/>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
-
-
-// en mode j'utilise map :
-
-// const characters = {
-//   {
-//   image = 'https://www.supersoluce.com/sites/default/files/styles/exact_size/public/node/127828/kannahommeportrait.png?itok=xraZxxLv',
-//   firstName = "Perrine",
-//   lastName = "Martyris"
-//   },
-//   {
-//     image = 'https://i.pinimg.com/originals/33/c5/2c/33c52c6a2a243bc785df4d2d6efbd9b3.png',
-//     firstName = "Toto",
-//     lastName = "Gogo"
-//   }
-//   {
-//     image = 'https://vignette2.wikia.nocookie.net/dgraymansp/images/6/6f/Allen_Cuerpo_Casi_Completo_(FanArt).png/revision/latest?cb=20130507224558&path-prefix=es',
-//     firstName = "Super",
-//     lastName = "Man"
-//   }
-// }
-
-
-//{characters.map((char, i) => {
-// return <Avatar {...char} />;
-// })}
--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -1,19 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 
-
-const Avatar = ({image, firstName, lastName}) => (
-    <div>
-        <h1>{firstName} {lastName.toUpperCase()}</h1>
-        <img src={image} alt="image"/>
-    </div>
+const Avatar = ({
+	image = "https://www.drupal.org/files/issues/default-avatar.png",
+	firstName,
+	lastName,
+	star
+}) => (
+	<div style={{ backgroundColor: star ? "#000" : "#666", width: "60vw" }}>
+		<h1>
+			{firstName} {lastName.toUpperCase()}
+		</h1>
+		<img src={image} alt="" />
+	</div>
 );
+
 export default Avatar;
-
-
-// const Avatar = props => {
-//     return <div>
-//         <h3>{props.firstName}</h3>
-//         <h3>{props.lastName}</h3>
-//         <img src={props.image} alt=""/>
-//     </div>
-// };
