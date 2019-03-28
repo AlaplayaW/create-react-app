04fef56328221ca0497eb481d25e90795c1dd1bc
Perrine Martyris <perrinemartyris@gmail.com>
2019-03-28 10:42:27 +0100

    react 3 finished and customized

--- src/App.js
+++ src/App.js
@@ -18,8 +18,11 @@ class App extends Component {
       isWorking: !this.state.isWorking,
       on: !this.state.on});
   };
+
+
   render() {
     const light = this.state.on ? 'Homer is working' : 'Homer is not working !';
+    const color = this.state.on ? '#3fc380' : '#f03434';
     return (
       <div className="App">
         <header className="App-header">
@@ -28,8 +31,7 @@ class App extends Component {
           : <img src={logo} className="App-logo" alt="logo" />}
           <h1 className="App-title">Simpsons Quotes</h1>
           <button 
-            className="App-change" 
-            onClick={this.handleClick}>{light.toUpperCase()}
+            onClick={this.handleClick} style={{backgroundColor : color}}>{light.toUpperCase()}
           </button>
         </header>
         <Quotes />
