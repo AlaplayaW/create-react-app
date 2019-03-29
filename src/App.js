b68008c709e47ba58d56eb91e3d76b766989fb38
Perrine Martyris <perrinemartyris@gmail.com>
2019-03-29 15:27:25 +0100

    start

--- src/App.js
+++ src/App.js
@@ -3,25 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (color, isWorking) {
+    super (color, isWorking)
+      this.state = {
+        color: "#FF66FF", 
+        isWorking: true
+      }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div style={{backgroundColor: this.state.color}}>la couleur est {this.state.color}</div>
+    )
   }
 }
 
